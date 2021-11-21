fc8964ad7f4550b3f6e4d12d2904ccb5a4f36a85
Reuben George <reuben_george1998@outlook.com>
2021-11-21 18:12:17 +0000

    projects-updated

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,4 +1,4 @@
-function getHamburgerMenu() {
+const getHamburgerMenu = () => {
   let nav = document.getElementById("nav-links-mobile");
   if (nav.style.display === "flex") {
     nav.style.display = "none";
@@ -7,3 +7,21 @@ function getHamburgerMenu() {
     nav.style.display = "flex";
   }
 }
+
+
+document.querySelector("#hangmanPreview").addEventListener("click", () => {
+  window.location = `https://r7george.github.io/hangman/`;
+});
+
+document.querySelector("#hangmanRepo").addEventListener("click", () => {
+  window.location = `https://github.com/r7george/hangman`;
+});
+
+
+document.querySelector("#calculatorPreview").addEventListener("click", () => {
+  window.location = `https://r7george.github.io/calculator/`;
+});
+
+document.querySelector("#calculatorRepo").addEventListener("click", () => {
+  window.location = `https://github.com/r7george/calculator`;
+});
